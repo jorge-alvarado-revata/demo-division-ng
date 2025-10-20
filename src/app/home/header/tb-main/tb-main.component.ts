2853783206d72d681b0cc3d967ab02db8e02a95d
jorge-alvarado-revata <serviciosplusapp@gmail.com>
2025-10-20 00:55:54 -0500

    primeros pasos de disenio front

--- src/app/home/header/tb-main/tb-main.component.ts
+++ src/app/home/header/tb-main/tb-main.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-tb-main',
-  imports: [],
+  imports: [NzButtonModule, NzDropDownModule, NzIconModule],
   templateUrl: './tb-main.component.html',
   styleUrl: './tb-main.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NzLayoutModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
